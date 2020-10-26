22138c568211c4173f125a8c0d43aebffe3787a6
NeProgramist <zaskozhenya@gmail.com>
2020-10-26 10:15:41 +0200

    feat(app): add basic impl of preferencesManager [./app/../PreferencesManager.kt]

---
+++ app/src/main/java/com/agb/lemon_android/frameworks/datasource/local/PreferencesManager.kt
@@ -0,0 +1,16 @@
+package com.agb.lemon_android.frameworks.datasource.local
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+
+class PreferencesManager(context: Context) {
+    private val preference = PreferenceManager.getDefaultSharedPreferences(context)
+
+    fun getString(name: String): String? = preference.getString(name, null)
+    fun putString(name: String, value: String?) {
+        preference
+            .edit()
+            .putString(name, value)
+            .apply()
+    }
+}
