ce808b172cde2b09e5bc9b24eb234e7ddd54daea
NeProgramist <zaskozhenya@gmail.com>
2020-11-01 18:48:43 +0200

    fix(app): change returned type to Result<*>

--- app/src/main/java/com/agb/lemon_android/frameworks/datasource/local/UserLocalDataSource.kt
+++ app/src/main/java/com/agb/lemon_android/frameworks/datasource/local/UserLocalDataSource.kt
@@ -1,14 +1,15 @@
 package com.agb.lemon_android.frameworks.datasource.local
 
+import com.agb.core.common.Result
 import com.agb.core.data.UserDataSource
 import com.agb.core.domain.model.User
 
 class UserLocalDataSource(private val prefManager: PreferencesManager) : UserDataSource {
-    override suspend fun getUserInfo(): User {
+    override suspend fun getUserInfo(): Result<User> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun saveUserInfo(user: User) {
+    override suspend fun saveUserInfo(user: User): Result<Unit> {
         TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/agb/lemon_android/frameworks/datasource/remote/UserRemoteDataSource.kt
+++ app/src/main/java/com/agb/lemon_android/frameworks/datasource/remote/UserRemoteDataSource.kt
@@ -1,15 +1,16 @@
 package com.agb.lemon_android.frameworks.datasource.remote
 
+import com.agb.core.common.Result
 import com.agb.core.data.UserDataSource
 import com.agb.core.domain.model.User
 import com.agb.lemon_android.frameworks.datasource.remote.api.LemonApi
 
 class UserRemoteDataSource(private val api: LemonApi) : UserDataSource {
-    override suspend fun getUserInfo(): User {
+    override suspend fun getUserInfo(): Result<User> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun saveUserInfo(user: User) {
+    override suspend fun saveUserInfo(user: User): Result<Unit> {
         TODO("Not yet implemented")
     }
 }
