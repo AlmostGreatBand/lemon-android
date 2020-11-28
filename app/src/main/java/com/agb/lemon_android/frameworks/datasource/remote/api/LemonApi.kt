3f0299cdccd2674e330941a72ba2aa7c742782ae
ALEGATOR1209 <kovalyshinO@gmail.com>
2020-11-28 14:43:05 +0200

    Updated LemonAPI

--- app/src/main/java/com/agb/lemon_android/frameworks/datasource/remote/api/LemonApi.kt
+++ app/src/main/java/com/agb/lemon_android/frameworks/datasource/remote/api/LemonApi.kt
@@ -5,19 +5,18 @@ import com.agb.core.domain.model.Transaction
 import com.agb.core.domain.model.User
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface LemonApi {
     @POST("profile")
-    suspend fun updateProfile(@Header("Authorization") credentials: String, @Body user: User)
+    suspend fun updateProfile(@Body user: User)
 
     @GET("profile")
-    suspend fun getProfile(@Header("Authorization") credentials: String): User
+    suspend fun getProfile(): User
 
     @GET("cards")
-    suspend fun getCards(@Header("Authorization") credentials: String): List<Card>
+    suspend fun getCards(): List<Card>
 
     @GET("transactions")
-    suspend fun getTransactions(@Header("Authorization") credentials: String): List<Transaction>
+    suspend fun getTransactions(): List<Transaction>
 }
